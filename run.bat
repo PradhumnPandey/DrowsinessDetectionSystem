571973080670a2dd2e75295abc6773f5669d6f28
PradhumnPandey <67554111+PradhumnPandey@users.noreply.github.com>
2022-09-10 01:24:07 +0530

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,4 @@
+cd C:\python\drowsiness-detection
+C:/Users/91784/Anaconda3/Scripts/activate
+conda activate base
+python drowsiness_detection_system.py --shape-predictor shape_predictor_68_face_landmarks.dat --alarm alarm.wav
